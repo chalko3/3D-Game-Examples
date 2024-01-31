e24c3f852055d0bbba0f78450b393de43b022512
chalko3 <113392114+chalko3@users.noreply.github.com>
2024-01-31 11:11:07 -0800

    Added Materials and Scripts For Movements

---
+++ 3D Game Examples/Assets/Scripts/RollABall.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;    
+
+public class RollABall : MonoBehaviour
+{
+    public float Speed = 10;
+    private Rigidbody _playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput;
+        float verticalInput;
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+        _playerRigidbody.AddForce(movement * Speed);
+    }
+}
