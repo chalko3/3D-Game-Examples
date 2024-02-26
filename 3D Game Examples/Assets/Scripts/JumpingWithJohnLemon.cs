791fae56778f2f4d48c790092f56f78bf19609bb
chalko3 <113392114+chalko3@users.noreply.github.com>
2024-02-26 11:40:16 -0800

    Obby Made

--- 3D Game Examples/Assets/Scripts/JumpingWithJohnLemon.cs
+++ 3D Game Examples/Assets/Scripts/JumpingWithJohnLemon.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class JohnLemonMovement : MonoBehaviour
 {
+    public int score = 0;
     public float turnSpeed = 20f;
     public float moveSpeed = 1f;
     public float JumpForce = 10f;
@@ -19,6 +21,7 @@ public class JohnLemonMovement : MonoBehaviour
     private Vector3 _defaultGravity = new Vector3(0f, -9.81f, 0f);
     private Vector3 _startingPosition;
     private Vector3 _checkpointPosition;
+    private GameObject[] _collectibles;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,7 @@ public class JohnLemonMovement : MonoBehaviour
         Physics.gravity = _defaultGravity;
         Physics.gravity *= GravityModifer;
         _startingPosition = transform.position;
+        _collectibles = GameObject.FindGameObjectsWithTag("Collectible-Return");
     }
 
     void Update()
@@ -41,10 +45,12 @@ public class JohnLemonMovement : MonoBehaviour
         {
             if(isAtCheckpoint)
             {
+                ReturningCollectibles();
                 transform.position = _checkpointPosition;
             }
             else
             {
+                ReturningCollectibles();
                 transform.position = _startingPosition;
             }
         }
@@ -76,6 +82,20 @@ public class JohnLemonMovement : MonoBehaviour
         {
             IsOnGround = true;
         }
+
+        if(collision.gameObject.CompareTag("Spinner"))
+        {
+            if(isAtCheckpoint)
+            {
+                ReturningCollectibles();
+                transform.position = checkpointAreaObject.transform.position;
+            }
+            else
+            {
+                ReturningCollectibles();
+                transform.position = _startingPosition;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -89,7 +109,29 @@ public class JohnLemonMovement : MonoBehaviour
         if(other.gameObject == finishAreaObject)
         {
             isAtCheckpoint = false;
+            ReturningCollectibles();
             transform.position = _startingPosition;
+        }
+
+        if(other.gameObject.CompareTag("Collectible-Destroy"))
+        {
+            score++;
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.CompareTag("Collectible-Return"))
+        {
+           score++;
+           other.gameObject.GetComponent<Collectibles>().HideCollectibles();
+        }
+    }
+
+    void ReturningCollectibles()
+    {
+        for(int i = 0; i < _collectibles.Length; i++)
+        {
+            _collectibles[i].SetActive(true);
+            _collectibles[i].GetComponent<Collectibles>().ReturnCollectibles();
         }
     }
 }
